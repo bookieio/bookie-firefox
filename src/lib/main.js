543f9e501e1db495151f4a36c57d26589b6e1837
Will Fuqua <wafuqua@gmail.com>
2012-02-25 15:22:59 -0500

    beginning of content/addon messaging

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,12 +1,21 @@
 const widgets = require("widget");
 const data = require('self').data;
 const panel = require("panel");
+const tabs = require("tabs");
 
 var popup = panel.Panel({
     contentURL: data.url("popup.html"),
+    //contentScript: "self.port.emit('show', 'panel is showing');",
     width:480,
-    height:300
+    height:300,
+    //onShow: function() {
+    //}
 });
+
+popup.port.on("show", function(arg) {
+        popup.port.emit("active", tabs.activeTab.title);
+        console.log("sent active tab " + tabs.activeTab.title);
+})
 
 var widget = widgets.Widget({
     id: "bookie-link",
@@ -15,3 +24,4 @@ var widget = widgets.Widget({
     panel: popup
 });
 
+console.log("init complete");
